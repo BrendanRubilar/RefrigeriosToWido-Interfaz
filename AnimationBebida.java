f456508f2dd811256edb0c5432c4c7f165b06a34
Brendan Rubilar <82827268+BrendanRubilar@users.noreply.github.com>
2022-11-07 22:01:00 -0300

    Mejora salida de bebida
    Animacion mejorada, se elimina el randomizer...

--- AnimationBebida.java
+++ AnimationBebida.java
@@ -1,7 +1,6 @@
 import javax.swing.JLabel;
 import java.lang.Thread;
-import java.util.Random;
-import java.lang.Object;
+
 
 public class AnimationBebida extends JLabel implements Runnable{
 
@@ -14,18 +13,12 @@ public class AnimationBebida extends JLabel implements Runnable{
     public void run(){
         
        while(target.getY()<500){
-        target.setLocation(target.getX(),target.getY()+15);
+        target.setLocation(target.getX(),target.getY()+25);
         try {
             Thread.sleep(60);
         } catch (Exception e) {
             System.out.println(e);
         }
-       }
-
-       if(target.getY()>=500){
-        Random r = new Random();
-        int positionX = r.nextInt(350-200)+200;
-        target.setLocation(positionX,500);
        }
    
     }
