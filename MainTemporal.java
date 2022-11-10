47ab08e716e4774a9506a9a17afb4d4ebbfbe151
TomoCid <109482634+TomoCid@users.noreply.github.com>
2022-11-09 23:15:31 -0300

    Copia seguridad
    Co-authored-by: D0ggos <D0ggos@users.noreply.github.com>

--- MainTemporal.java
+++ MainTemporal.java
@@ -1,16 +1,9 @@
-package Tarea3;
-
 public class MainTemporal {
 
     public static void main(String[] args) {
-       
-        //EL MAIN SOLAMENTE GENERA UNA VENTANA, TODO LO DEMAS SE GENERA DENTRO...
+
         Ventana ventana1 = new Ventana();
         ventana1.setVisible(true);
-        
-        
-                
-    
 
     }
 }
